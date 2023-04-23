155ce7829db420fd8c58528bf57b0745dd160528
McCambley <jakemccamb16@gmail.com>
2023-04-23 09:48:44 -0400

    🏷 Type transform punctuation function

--- src/utils/transformPunctuation.ts
+++ src/utils/transformPunctuation.ts
@@ -1,3 +1,9 @@
+/**
+ *  Replaces punctuation keywords with their corresponding symbols
+ *
+ * @param input A strong created from spoken work that may contain punctuation keywords
+ * @returns A string with punctuation keywords replaced with their corresponding symbols
+ */
 export default function transformPunctuation(input: string) {
   const replacements = [
     { keyword: "comma", symbol: "," },
